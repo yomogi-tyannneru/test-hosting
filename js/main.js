3ccdc499dce7dae989985fc5d4967ca00f05ac30
松田晴登 <haruto.matsuda@gmail.com>
2021-07-25 20:52:04 +0900

    修正内容が分かるコミットメッセージ

--- js/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  alert('ページが読み込まれました');
-});
-
-alert('ページが読み込まれました');
-const button = document.querySelector("button");
-button.addEventListener("click", function () {
-  alert("クリックされました");
-});
